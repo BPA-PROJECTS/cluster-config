28710e4f15bfb9fb09b13d5d8084136b2a55f55c
krylov-sergey-sergeevich <krylov.sergey.sergeevich@gmail.com>
2024-08-20 12:32:29 +0300

    Add script load projects

---
+++ load_all_projects.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git clone git@github.com:BPA-PROJECTS/tg-platform.git
+
+git clone git@github.com:BPA-PROJECTS/tg-datalab.git
+
+git clone git@github.com:BPA-PROJECTS/tg-tech-monitor-bot.git
